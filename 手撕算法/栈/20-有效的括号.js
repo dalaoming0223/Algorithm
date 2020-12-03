9da6a367f243019cb582d1ef062e4c782abe1732
dalaoming <779507434@qq.com>
2020-12-03 09:59:15 +0800

    add queue

--- 手撕算法/栈/20-有效的括号.js
+++ 手撕算法/栈/20-有效的括号.js
@@ -22,3 +22,28 @@ var isValid = function(s) {
   }
   return !leftArr.length
 };
+
+
+var isValid = function(s) {
+  let stack = []
+  for (n in s){
+      
+      let gg = s[n];
+      if(gg === '(' || gg === '[' || gg === '{' ){
+          stack.push(gg)
+      }else{
+          let mm = stack[stack.length - 1]
+          if((mm==="(" && gg===')') || 
+             (mm==="[" && gg===']') ||
+             (mm==="{" && gg==='}')
+          ){
+              stack.pop()
+          }else{
+              return false
+          }
+      }
+
+
+  }
+  return stack.length === 0
+};
---
+++ 手撕算法/队列/基本方法.js
@@ -0,0 +1,6 @@
+const queue = []
+queue.push(1)
+queue.push(2)
+queue.push(3)
+const item1 = queue.shift() //1
+const item2 = queue.shift() //2
